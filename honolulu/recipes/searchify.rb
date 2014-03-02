1d649e48e3ae25f7624181d521d3a12838dc8aa8
Brian Jakovich <brian.jakovich@stelligent.com>
2014-03-02 15:42:11 -0500

    reindex w/ env vars

--- honolulu/recipes/searchify.rb
+++ honolulu/recipes/searchify.rb
@@ -1,3 +1,13 @@
+include_recipe "deploy"
+
+node[:deploy].each do |application, deploy|
+  deploy[:environment_variables].each do |key, value|
+    Chef::Log.info("Setting ENV[#{key}] to #{value}")
+    ENV[key] = value
+  end
+end
+
+
 execute "rake tanker:reindex" do
   cwd "/srv/www/honoluluanswers/current"
 end
