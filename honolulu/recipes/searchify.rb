636886fd2657e3b2b7b2c38f7f51273dd9162aaa
Brian Jakovich <brian.jakovich@stelligent.com>
2014-03-02 15:56:51 -0500

    log -> Log

--- honolulu/recipes/searchify.rb
+++ honolulu/recipes/searchify.rb
@@ -1,7 +1,7 @@
 include_recipe "deploy"
 
 node[:deploy].each do |application, deploy|
-  Chef::log.info("Deploy DIR: #{deploy[:deploy_to]}")
+  Chef::Log.info("Deploy DIR: #{deploy[:deploy_to]}")
 end
 
 
