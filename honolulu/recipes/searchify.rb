5cae7243c76717d82a7d8adb9362274fc4f1b142
Brian Jakovich <brian.jakovich@stelligent.com>
2014-03-02 15:45:02 -0500

    Checking deploy_to log

--- honolulu/recipes/searchify.rb
+++ honolulu/recipes/searchify.rb
@@ -1,10 +1,7 @@
 include_recipe "deploy"
 
 node[:deploy].each do |application, deploy|
-  deploy[:environment_variables].each do |key, value|
-    Chef::Log.info("Setting ENV[#{key}] to #{value}")
-    ENV[key] = value
-  end
+  Chef::log.info("Deploy DIR: #{deploy[:deploy_to]}")
 end
 
 
